27d298dc143b058f2a1bc8f264607abf6c21ecfb
João Victor Inácio <j.inacio.s@hotmail.com>
2025-03-21 12:32:40 -0300

    adicionando comportamento de abrir e fechar da sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,33 @@
 if (module.hot) {
-    module.hot.accept();
-  }
+	module.hot.accept();
+}
 
-import "../styles/base/reset.css"
-import "../styles/base/variables.css"
-import "../styles/base/typography.css"
-import "../styles/main.css"
+import "../styles/base/reset.css";
+import "../styles/base/variables.css";
+import "../styles/base/typography.css";
+import "../styles/main.css";
 
+const menuBtn = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".mobile-sidebar");
+const closeSidebarBtn = document.querySelector('.x-icon')
 
+console.log(closeSidebarBtn)
 
-document.addEventListener('DOMContentLoaded', () => {
-    // const h1 = document.createElement("h1")
-    // h1.innerText = "Olá, mundo"
-    // h1.id = "titulo"
-    // h1.className = "inter-bold"
-    // document.body.appendChild(h1)
+closeSidebarBtn.addEventListener('click', () => {
+    sidebar.style.display = "none"
 })
+
+menuBtn.addEventListener("click", () => {
+	sidebar.style.display = "block"
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	// const h1 = document.createElement("h1")
+	// h1.innerText = "Olá, mundo"
+	// h1.id = "titulo"
+	// h1.className = "inter-bold"
+	// document.body.appendChild(h1)
+});
 
 const carousel = document.querySelector(".carousel");
 const images = document.querySelectorAll(".carousel img");
@@ -26,33 +38,33 @@ let index = 0;
 let interval;
 
 function showImage(i) {
-    if (i < 0) {
-        index = images.length - 1;
-    } else if (i >= images.length) {
-        index = 0;
-    } else {
-        index = i;
-    }
-    carousel.style.transform = `translateX(-${index * 100}%)`;
+	if (i < 0) {
+		index = images.length - 1;
+	} else if (i >= images.length) {
+		index = 0;
+	} else {
+		index = i;
+	}
+	carousel.style.transform = `translateX(-${index * 100}%)`;
 }
 
 function nextImage() {
-    showImage(index + 1);
+	showImage(index + 1);
 }
 
 function startAutoSlide() {
-    clearInterval(interval);
-    interval = setInterval(nextImage, 5000);
+	clearInterval(interval);
+	interval = setInterval(nextImage, 5000);
 }
 
 prevButton.addEventListener("click", () => {
-    showImage(index - 1);
-    startAutoSlide();
+	showImage(index - 1);
+	startAutoSlide();
 });
 
 nextButton.addEventListener("click", () => {
-    showImage(index + 1);
-    startAutoSlide();
+	showImage(index + 1);
+	startAutoSlide();
 });
 
 startAutoSlide();
